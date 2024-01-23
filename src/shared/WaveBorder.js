7abbde8daa99f780bb140c8f9680bac8f732aeb6
vitaliyMiskevuch1212 <miskotop1234@gmail.com>
2024-01-23 09:35:05 +0200

    Update README.md (WaveBorder.js)

--- src/shared/WaveBorder.js
+++ src/shared/WaveBorder.js
@@ -22,7 +22,7 @@ const styles = theme => ({
   parallax: {
     "& > use": {
       animation: "$moveForever 4s cubic-bezier(0.62, 0.5, 0.38, 0.5) infinite",
-      animationDelay: props => `-${props.animationNegativeDelay}s`
+      animationDelay: "-2s"
     }
   }
 });
@@ -61,7 +61,6 @@ WaveBorder.propTypes = {
   lowerColor: PropTypes.string.isRequired,
   upperColor: PropTypes.string.isRequired,
   classes: PropTypes.object.isRequired,
-  animationNegativeDelay: PropTypes.number.isRequired,
   className: PropTypes.string
 };
 
